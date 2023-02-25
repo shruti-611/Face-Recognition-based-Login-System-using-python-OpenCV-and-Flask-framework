38946f971a02c0bf000b3a4987ed20f725c333f6
Shruti Gujar <91453214+shruti-611@users.noreply.github.com>
2023-02-26 02:59:40 +0530

    Add files via upload

---
+++ Detectface.py
@@ -0,0 +1,141 @@
+
+#Shruti Gujar
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox as ms
+import cv2,os
+import shutil
+import csv
+import numpy as np
+from PIL import Image, ImageTk
+import pandas as pd
+
+from flask import Flask
+app = Flask(__name__)
+
+
+# This will make sure no duplicates exixts in profile.csv(using Pandas here)
+df = pd.read_csv('Profile.csv')
+df.sort_values('Ids', inplace = True)
+df.drop_duplicates(subset = 'Ids', keep = 'first', inplace = True)
+df.to_csv('Profile.csv', index = False)
+
+# Fuction to detect the face
+
+@app.route('/', methods=["GET"])
+def DetectFace():
+    reader = csv.DictReader(open('Profile.csv'))
+    print('Detecting Login Face')
+    for rows in reader:
+        result = dict(rows)
+        #print(result)
+        if result['Ids'] == '1':
+            name1 = result['Name']
+        elif result['Ids'] == '2':
+            name2 = result["Name"]
+    recognizer = cv2.face.LBPHFaceRecognizer_create()  #cv2.createLBPHFaceRecognizer()
+    recognizer.read("TrainData\Trainner.yml")
+    # harcascadePath = "hh.xml"
+    # faceCascade = cv2.CascadeClassifier(harcascadePath);
+    faceCascade  = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    cam = cv2.VideoCapture(0)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    Face_Id = ''
+    name2 = ''
+
+    # Camera ON Everytime
+    while True:
+        ret, frame = cam.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(gray, 1.3, 5)
+        Face_Id = 'Not detected'
+
+        # Drawing a rectagle around the face 
+        for (x, y, w, h) in faces:
+            Face_Id = 'Not detected'
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            Id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+            if (confidence < 80):
+                if (Id == 1):
+                    name = name1
+                    
+                elif (Id == 2):
+                    name = name2
+                    
+                Predicted_name = str(name)
+                Face_Id = Predicted_name
+            else:
+                Predicted_name = 'Unknown'
+                Face_Id = Predicted_name
+                # Here unknown faces detected will be stored
+                noOfFile = len(os.listdir("UnknownFaces")) + 1
+                if int(noOfFile) < 100:
+                    cv2.imwrite("UnknownFaces\Image" + str(noOfFile) + ".jpg", frame[y:y + h, x:x + w])
+                
+                else:
+                    pass
+
+
+            cv2.putText(frame, str(Predicted_name), (x, y + h), font, 1, (255, 255, 255), 2)
+            
+        cv2.imshow('Picture', frame)
+        #print(Face_Id)
+        cv2.waitKey(1)
+
+        # Checking if the face matches for Login
+        if Face_Id == 'Not detected':
+            print("-----Face Not Detected, Try again------")
+            pass
+
+            
+        elif Face_Id == name1 or name2 and Face_Id != 'Unknown' :
+            print('----------Detected as {}----------'.format(name1))
+            print('-----------login successfull-------')
+            print('***********WELCOME {}**************'.format(name1))
+          
+        
+            # system = tk.Tk()
+
+
+            # system.geometry('1280x900')
+
+
+            # system.configure(background='black')
+
+
+            # system.resizable(width=False, height=False)
+
+
+            # system.title('Login and Registration System')
+
+            
+
+            # system.wm_title('!! CRIMINAL DETECTION AND RECOGNITION !!')
+
+
+            # top_frame = Label(system, text='!! LOGIN SUCCESSFUL !!',font = ('Cosmic', 25, 'bold'), bg='#7268A6',relief='groove',padx=500, pady=30)
+            # top_frame.pack(side='top')
+
+
+
+            # canvas = Canvas(system, width=500, height=350)
+            
+            # system.mainloop()
+
+            return 'User '+name+' has been logged in successfully'
+
+            break
+        else:
+            print('-----------Login failed please try again-------')
+            return 'User has been logged in successfully'
+        
+        
+        #if (cv2.waitKey(1) == ord('q')):
+        #   break
+        return 'User has been logged in successfully'
+
+if __name__ == "__main__":
+    app.run()
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Face-Recognition-based-Login-System-using-python-OpenCV-and-Flask-framework
+## Face-Login
+Face login using Open CV Python
+
+
+
+### Installation
+
+python 3.8.2
+
+pip install opencv-python
+pip install opencv-contrib-python
+
+pip install pillow
+
+pip install pandas
+
+pip install numpy
+
+## Prerequisites
+
+Numpy </br>
+OpenCV </br>
+Pillow </br>
+Pandas</br>
+
+
+## Process :-
+
+1. Create the same folders : testing or download entire project
+2. Run Readface.py : take some images of user for login,id and name needs to be added
+3. Run Train.py : this will train all images taken and put in TrainData Folder......
+4. Run Detectface.py to detect the face and Login.......
+
