cc4d27befeb0e4223751c13487070622323bde8f
Shruti Gujar <91453214+shruti-611@users.noreply.github.com>
2023-02-26 13:15:30 +0530

    Update Detectface.py

--- Detectface.py
+++ Detectface.py
@@ -1,5 +1,6 @@
 
 #Shruti Gujar
+
 import tkinter as tk
 from tkinter import *
 from tkinter import messagebox as ms
@@ -14,13 +15,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-# This will make sure no duplicates exixts in profile.csv(using Pandas here)
+
 df = pd.read_csv('Profile.csv')
 df.sort_values('Ids', inplace = True)
 df.drop_duplicates(subset = 'Ids', keep = 'first', inplace = True)
 df.to_csv('Profile.csv', index = False)
 
-# Fuction to detect the face
+
 
 @app.route('/', methods=["GET"])
 def DetectFace():
@@ -35,22 +36,19 @@ def DetectFace():
             name2 = result["Name"]
     recognizer = cv2.face.LBPHFaceRecognizer_create()  #cv2.createLBPHFaceRecognizer()
     recognizer.read("TrainData\Trainner.yml")
-    # harcascadePath = "hh.xml"
-    # faceCascade = cv2.CascadeClassifier(harcascadePath);
+
     faceCascade  = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     cam = cv2.VideoCapture(0)
     font = cv2.FONT_HERSHEY_SIMPLEX
     Face_Id = ''
     name2 = ''
 
-    # Camera ON Everytime
     while True:
         ret, frame = cam.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = faceCascade.detectMultiScale(gray, 1.3, 5)
         Face_Id = 'Not detected'
-
-        # Drawing a rectagle around the face 
+ 
         for (x, y, w, h) in faces:
             Face_Id = 'Not detected'
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
@@ -67,7 +65,7 @@ def DetectFace():
             else:
                 Predicted_name = 'Unknown'
                 Face_Id = Predicted_name
-                # Here unknown faces detected will be stored
+            
                 noOfFile = len(os.listdir("UnknownFaces")) + 1
                 if int(noOfFile) < 100:
                     cv2.imwrite("UnknownFaces\Image" + str(noOfFile) + ".jpg", frame[y:y + h, x:x + w])
@@ -82,16 +80,15 @@ def DetectFace():
         #print(Face_Id)
         cv2.waitKey(1)
 
-        # Checking if the face matches for Login
         if Face_Id == 'Not detected':
             print("-----Face Not Detected, Try again------")
             pass
 
             
         elif Face_Id == name1 or name2 and Face_Id != 'Unknown' :
-            print('----------Detected as {}----------'.format(name1))
+           
             print('-----------login successfull-------')
-            print('***********WELCOME {}**************'.format(name1))
+           
           
         
             # system = tk.Tk()
